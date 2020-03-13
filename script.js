fc65ae423cc382fb4f4501a6269f7b31ea442c9f
lukless1990 <lukas.gehrig@yahoo.de>
2020-03-13 22:02:25 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -23,7 +23,26 @@ var output = document.getElementById("demo");
 
 var selYear = slider.value;
 
-generatePlot()
+
+var innerContainer = document.querySelector('[data-num="0"');
+// plotEl = innerContainer.querySelector('.plot');
+tdSelector = innerContainer.querySelector('.tddata');
+
+var tdOption = document.createElement('option');
+tdOption.text = 'Disciplines';
+tdSelector.appendChild(tdOption);
+var tdOption = document.createElement('option');
+tdOption.text = 'Objects';
+tdSelector.appendChild(tdOption);
+var tdOption = document.createElement('option');
+tdOption.text = 'Techniques';
+tdSelector.appendChild(tdOption);
+
+var selValue = tdSelector.value;
+
+
+
+
 
 output.innerHTML = slider.value; // Display the default slider value
 
@@ -36,6 +55,7 @@ slider.oninput = function() {
 
 }
 
+tdSelector.addEventListener('change', generatePlot, false);
 
 
 
@@ -77,21 +97,15 @@ function generatePlot() {
 
 
 function getItems(input) {
-  // var arr = input;
-  var arr = [];
 
-  // console.log(selYear,'test');
-  // console.log( (new Date(parseInt(selYear)+1,8-1,31)));
+  selValue = tdSelector.value
+
+  var arr = [];
 
   for (var i = 0; i < input.length; i++) {
     sd = (input[i].start_date).split(";")[0].split("-")
 
-    // console.log(new Date(sd[0],sd[1]-1,sd[2]), new Date(selYear,9-1,1) );
-    //
-    // console.log((new Date(sd[0],sd[1]-1,sd[2]) >= new Date(parseInt(selYear),9-1,1) ));
-    // console.log((new Date(sd[0],sd[1]-1,sd[2]) < new Date(parseInt(selYear)+1,8-1,31)));
     if ( (new Date(sd[0],sd[1]-1,sd[2]) >= new Date(parseInt(selYear),9-1,1) )  &&  (new Date(sd[0],sd[1]-1,sd[2]) < new Date(parseInt(selYear)+1,8-1,31)) ) {
-
       arr.push(input[i])
     }
 
@@ -99,34 +113,28 @@ function getItems(input) {
 
 
 
-  var fin = [], dis = {}, obj = {}, teq = {} ;
+  var fin = {} ;
+
+  if (selValue == 'Disciplines') {
+    var selGr = 'disciplines'
+  } else if (selValue == 'Objects') {
+    var selGr = 'tadirah_objects'
+  } else if (selValue == 'Techniques') {
+    var selGr = 'tadirah_techniques'
+
+  }
+
   for (var i = 0; i < arr.length; i++) {
 
-    for (var j = 0; j < arr[i].disciplines.length; j++) {
-        if (!dis[arr[i].disciplines[j].name]) {
-          dis[arr[i].disciplines[j].name] = 1;
-        } else if (dis[arr[i].disciplines[j].name]) {
-          dis[arr[i].disciplines[j].name] += 1;
-        }
-      };
-    for (var j = 0; j < arr[i].tadirah_objects.length; j++) {
-        if (!obj[arr[i].tadirah_objects[j].name]) {
-          obj[arr[i].tadirah_objects[j].name] = 1;
-        } else if (obj[arr[i].tadirah_objects[j].name]) {
-          obj[arr[i].tadirah_objects[j].name] += 1;
-        }
-      };
-    for (var j = 0; j < arr[i].tadirah_techniques.length; j++) {
-        if (!teq[arr[i].tadirah_techniques[j].name]) {
-          teq[arr[i].tadirah_techniques[j].name] = 1;
-        } else if (teq[arr[i].tadirah_techniques[j].name]) {
-          teq[arr[i].tadirah_techniques[j].name] += 1;
+    for (var j = 0; j < arr[i][selGr].length; j++) {
+        if (!fin[arr[i][selGr][j].name]) {
+          fin[arr[i][selGr][j].name] = 1;
+        } else if (fin[arr[i][selGr][j].name]) {
+          fin[arr[i][selGr][j].name] += 1;
         }
       }
   };
-  fin.push(dis);
-  fin.push(obj);
-  fin.push(teq);
+
   return fin;
 
 }
@@ -140,132 +148,17 @@ function getItems(input) {
 function plot(dd){
 
 
-  function makePlotData(i) {
-
-// 0:disciplines, 1:objects, 2:techniques
 
     let x = [];
     let y = [];
 
-    for (var p in dd[i]) {
+    for (var p in dd) {
           x.push(p);
-          y.push(dd[i][p]);
+          y.push(dd[p]);
         }
 
-    return {x,y, visible: i === 0, type:'bar'};
-  }
-
-
-    Plotly.newPlot('graph', [0,1,2].map(makePlotData), {
-
-       // sliders: [{
-       //       pad: {t: 30},
-       //       x: 0.05,
-       //       y: -1,
-       //       len: 0.95,
-       //       currentvalue: {
-       //         xanchor: 'right',
-       //         prefix: 'Academic year starting in September of ',
-       //         font: {
-       //           color: '#888',
-       //           size: 20
-       //         }
-       //       },
-       //
-       //       steps: [{
-       //         label: '1999',
-       //         method: 'restyle',
-       //         args: [['red'], {
-       //           mode: 'immediate',
-       //           frame: {redraw: false}
-       //         }]
-       //       }, {
-       //         label: '2000',
-       //         method: 'restyle',
-       //         args: [['green'], {
-       //           mode: 'immediate',
-       //           frame: {redraw: false}
-       //         }]
-       //       }, {
-       //         label: '2001',
-       //         method: 'restyle',
-       //         args: [['blue'], {
-       //           mode: 'immediate',
-       //           frame: {redraw: false}
-       //         }]
-       //       }]
-       //     }],
-
-      updatemenus: [ {
-          y: 1,
-          yanchor: 'top',
-          buttons: [{
-              method: 'restyle',
-              args: ['visible', [true, false, false]],
-              label: 'Disciplines'
-          }, {
-              method: 'restyle',
-              args: ['visible', [false, true, false]],
-              label: 'Objects'
-          }, {
-              method: 'restyle',
-              args: ['visible', [false, false, true]],
-              label: 'Techniques'
-          }]
-      }],
-  });
-
-
-
+    Plotly.newPlot('graph', [{x,y,  type:'bar'}] );
 
 }
 
-
-
-
-
-
-
-//
-// var rawDataURL = 'https://raw.githubusercontent.com/plotly/datasets/master/2016-weather-data-seattle.csv';
-// var xField = 'Date';
-// var yField = 'Mean_TemperatureC';
-//
-//
-//
-// Plotly.d3.csv(rawDataURL, function(err, rawData) {
-//     if(err) throw err;
-//
-//     var data = prepData(rawData);
-//     var layout = {
-//         title: 'Time series with range slider and selectors',
-//         xaxis: {
-//             rangeslider: {}
-//         },
-//         yaxis: {
-//             fixedrange: true
-//         }
-//     };
-//
-//     Plotly.plot('graph', data, layout, {showSendToCloud: true});
-// });
-//
-// function prepData(rawData) {
-//     var x = [];
-//     var y = [];
-//
-//     console.log(rawData.length)
-//
-//     rawData.forEach(function(datum, i) {
-//         if(i % 100) return;
-//
-//         x.push(new Date(datum[xField]));
-//         y.push(datum[yField]);
-//     });
-//
-//     return [{
-//         mode: 'lines',
-//         x: x,
-//         y: y
-//     }];
-// }
+generatePlot()
