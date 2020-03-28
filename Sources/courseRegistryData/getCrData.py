ac32ad6c960183016c3559a4bc43f4888ea6dc12
bellerophons-pegasus <bellerophons_pegasus@yahoo.de>
2020-03-29 00:56:10 +0100

    Updating script to get data in format for chord diagram

--- Sources/courseRegistryData/getCrData.py
+++ Sources/courseRegistryData/getCrData.py
@@ -1,59 +1,38 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+########################################################################
 Created on Wed Mar 25 12:36:44 2020
 
+A script to load the DH Course Registry data into a .js file.
+It also checks if the start dates are in the correct format: (%Y-%m-%d).
+If the desired date format is not used it will be corrected accordingly.
+A course without any date information is not included into the dataset.
+The whole dataset is saved into the variable 'data' in the file index.js.
+
 @author: lukas
+########################################################################
 """
-
-####
-####
-####
-#
-#   This script loads the Course Registry data in a .js file. Additionally it checks if the start dates are in the correct format
-#   if not the desired format (%Y-%m-%d) is used, it will be corrected and the whole dataset is saved in the file index.js
-#
-####
-####
-####
-
-
-#### only some basic modules are used
-
+# Import of needed libraries
 import urllib.request
 import json
 from datetime import datetime
 
-
-
-
-#### the variable in which the data is initially saved
-
+# The variable in which the data from the API is initially saved
 data_tot = []
 
-
-#### requesting the data via the Course Registry API
-
+# Request the data via the Course Registry API
 with urllib.request.urlopen("https://dhcr.clarin-dariah.eu/api/v1/courses/index") as url:
         data = json.loads(url.read().decode())
-        
-        #### Checking if the start_date input is formatted correctly
-        
+        # Check if the start_date input is formatted correctly
         for j in data:
-            
-            #### if there is no start_date specified, the respective entry will be omitted
-            
+            # If no start_date is specified, the respective entry will be omitted
             if j['start_date']=='' or j['start_date']==None:
                 continue
-            
             else:
-                
-                #### string for the new (corrected) start_date
-                
+                # Variable for the new (corrected) start_date
                 new_date_str = ''
-                
-                #### in case several start_dates are given
-                
+                # In case several start_dates are given process each one
                 for dd in j['start_date'].split(';'):
                     try:
                         new_date = datetime.strptime(dd, '%Y-%m-%d')
@@ -62,25 +41,16 @@ with urllib.request.urlopen("https://dhcr.clarin-dariah.eu/api/v1/courses/index"
                         new_date = datetime.strptime(dd, '%Y-%m')
                         new_date = new_date.strftime('%Y-%m-%d')
                         new_date_str += new_date
-                        
-                #### if several dates were given, the ';' has to be placed again for correct formatting
-                
+                # If several dates were given, the ';' has to be placed again for correct formatting
                 if len(new_date_str) > 10:
                     for i in range(int(len(new_date_str)/10)-1,0,-1):
                         new_date_str = new_date_str[:i*10] +  ';'+ new_date_str[i*10:]
-                        
-                #### updating the start_date for each entry 
-                
+                # Update the start_date for each entry
                 j['start_date'] = new_date_str
-          
-                #### appending the entry to the data variable
-                
+                # Append the entry to the data variable
                 data_tot.append(j)
-            
-#### finally the new file is written
-         
+
+# Finally the js-file is written
 with open('index.js', 'w') as outfile:
     outfile.write('var data = ')
     json.dump(data_tot, outfile)
-
-            
--- Sources/courseRegistryData/getCrDataMat.py
+++ Sources/courseRegistryData/getCrDataMat.py
@@ -1,56 +1,63 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+########################################################################
 Created on Wed Mar 25 12:36:44 2020
 
-@author: lukas
+A script to count co-occurences of the keywords for disciplines, objects,
+and techniques used in all the recent courses of the DH Course Registry.
+Data is harvested via the DHCR API and then transformed into a dictionary,
+which is further printed into a JSON file for further use.
+
+Example output:
+{
+	"Archaeology": [{
+			"coocurrences": [{
+				"Archaeology": 0
+			}, {
+				"Arts and Cultural Studies": 20
+			}]
+		},
+		{
+			"courses": ["id1", "id2"]
+		}
+	],
+	"Arts": [{
+			"coocurrences": [{
+				"Archaeology": 20
+			}, {
+				"Arts and Cultural Studies": 0
+			}]
+		},
+		{
+			"courses": ["id2", "id3"]
+		}
+	]
+}
+
+@author: lukas & martina
+########################################################################
 """
-
-####
-####
-####
-#
-#   This script generates a co-occurence matrix for the disciplines/objects/techniques from all recent courses
-#
-####
-####
-####
-
-
-#### only some basic modules are used
-
+# Import of needed libraries
 import urllib.request
 import json
 from datetime import datetime
 
-
-#### the variable in which the data is initially saved
-
+# The variable in which the data from the API is initially saved
 data_tot = []
 
-
-#### requesting the data via the Course Registry API; only recent ones
-
+# Request the data via the Course Registry API; only recent ones
 with urllib.request.urlopen("https://dhcr.clarin-dariah.eu/api/v1/courses/index?recent") as url:
         data = json.loads(url.read().decode())
-        
-        #### Checking if the start_date input is formatted correctly
-        
+        # Check if the start_date input is formatted correctly
         for j in data:
-            
-            #### if there is no start_date specified, the respective entry will be omitted
-            
+            # If no start_date is specified, the respective entry will be omitted
             if j['start_date']=='' or j['start_date']==None:
                 continue
-            
             else:
-                
-                #### string for the new (corrected) start_date
-                
+                # Variable for the new (corrected) start_date
                 new_date_str = ''
-                
-                #### in case several start_dates are given
-                
+                # In case several start_dates are given process each one
                 for dd in j['start_date'].split(';'):
                     try:
                         new_date = datetime.strptime(dd, '%Y-%m-%d')
@@ -59,83 +66,86 @@ with urllib.request.urlopen("https://dhcr.clarin-dariah.eu/api/v1/courses/index?
                         new_date = datetime.strptime(dd, '%Y-%m')
                         new_date = new_date.strftime('%Y-%m-%d')
                         new_date_str += new_date
-                        
-                #### if several dates were given, the ';' has to be placed again for correct formatting
-                
+                # If several dates were given, the ';' has to be placed again for correct formatting
                 if len(new_date_str) > 10:
                     for i in range(int(len(new_date_str)/10)-1,0,-1):
-                        new_date_str = new_date_str[:i*10] +  ';'+ new_date_str[i*10:]
-                        
-                #### updating the start_date for each entry 
-                
+                        new_date_str = new_date_str[:i*10]+';'+ new_date_str[i*10:]
+                # Update the start_date for each entry
                 j['start_date'] = new_date_str
-          
-                #### appending the entry to the data variable
-                
+                # Append the entry to the data variable
                 data_tot.append(j)
-            
-            
-            
-#### for simplicity, a list is created in which all the keywords from each item 
-#### are stored
-keyWordList = []          
 
-#### lists for disciplines/objects/techniques and a total list for all keywords   
+# A list to store lists with all keywords from each item of the course list
+keyWordList = []
+# TODO: hier könnte man noch course id speichern
+
+# Lists for unique disciplines, objects, techniques, and a list for all together
 disList = []
 objList = []
 teqList = []
 totList = []
 
-#### add all keywords to the respective lists
+# Add all keywords to their respective lists
 for i in data_tot:
     keyWordList.append([])
     for j in i['disciplines']:
         keyWordList[-1].append(j['name'])
         if j['name'] not in disList:
-            disList.append(j['name'])    
+            disList.append(j['name'])
     for j in i['tadirah_objects']:
         keyWordList[-1].append(j['name'])
         if j['name'] not in objList:
-            objList.append(j['name'])       
+            objList.append(j['name'])
     for j in i['tadirah_techniques']:
         keyWordList[-1].append(j['name'])
         if j['name'] not in teqList:
-            teqList.append(j['name'])               
+            teqList.append(j['name'])
 
-#### sort the list alphabetically; also with respect to capital letters
+# Sort the list alphabetically; also with respect to capital letters
 disList = sorted(disList, key=lambda L: (L.lower(), L))
 objList = sorted(objList, key=lambda L: (L.lower(), L))
 teqList = sorted(teqList, key=lambda L: (L.lower(), L))
 
+# Add individual lists to the list with all keywords
 totList = disList + objList + teqList
 
-
-#### define a dict 
-#### for each keyword a new dict is added with all keywords 
-#### initially they have the value 0
-#### may also be written into a variable for later usage
+# We create a dictionary -> {}
+# For each keyword a dictionary that will contain values for co-occurences,
+# course ids, keyword groups, and categories is added.
+# --> {'Archaeology':{'coocurrences': {}, 'color': '',
+#                    'group': '', 'category': ''}}
+# For the co-occurence count a dictionary with each keyword is added
+# Initially the values are all set to 0
+# --> {'Archaeology':{'coocurrences': {'Archaeology': 0, ...}, 'color': '',
+#                    'group': '', 'category': ''}}
 totDict = {}
-
 for i in totList:
-    totDict[i] = {}
+    totDict[i] = {'coocurrences': {}, 'color': ''}
+    if i in disList:
+        totDict[i]['color'] = '#c54c82'
+    elif i in objList:
+        totDict[i]['color'] = '#003365'
+    elif i in teqList:
+        totDict[i]['color'] = '#609f60'
+    else:
+        totDict[i]['color'] = '#9a9a9a'
     for j in totList:
-        totDict[i][j] = 0
+        totDict[i]['coocurrences'][j] = 0
+#        print(totDict[i]['coocurrences'])
 
-#### now each item in keywordlist will be checked and the respective number 
-#### are increased
-
+# Now each list in keywordlist will be checked and the count of the respective
+# co-occurence pair in totDict is increased
 for i in keyWordList:
-    for j in i: 
-        for k in i:  
-            if k!=j:   
-                totDict[j][k] +=1
+    for j in i:
+        for k in i:
+            # Only increase if keyword is not co-occuring with itself
+            if k!=j:
+                totDict[j]['coocurrences'][k] +=1
 
 #### now the matrix will be defined as follows:
 #
-#
 #       [  [0, 0, ...., 0, 0],   //row for disList[0]
 #          [0, 0, ...., 0, 0],   //row for disList[1]
-#          .
 #          .
 #          .
 #          [0, 0, ...., 0, 0],   //row for teqList[-2]
@@ -148,41 +158,38 @@ totMat = []
 for i in totList:
     totMat.append([])
     for j in totList:
-        totMat[-1].append(totDict[i][j])
+        totMat[-1].append(totDict[i]['coocurrences'][j])
 
 
-#### for the d3 code the color variable can also be generated here
-
+# For the d3 code the color variable can also be generated here
 colors = []
 for i in disList:
-    colors.append('#FFFF00ff')
+    colors.append('#c54c82')
 for i in objList:
-    colors.append('#0000FFff')
+    colors.append('#003365')
 for i in teqList:
-    colors.append('#008000ff')            
+    colors.append('#609f60')
+
+stop = len(colors)
 
 
-stop = len(colors)      
-
-
-#### finally the new file is written
-         
+# Finally the js-file is written
 with open('dataMat.js', 'w') as outfile:
     outfile.write('var dataMat = [ \n')
     for i in range(len(totMat)):
-        
         outfile.write(str(totMat[i][0:stop+1]) + ', //'+ totList[i] +' \n')
         if i == stop:
             break
     outfile.write(']; \n')
-        
-        
-        
-    outfile.write('var totColors =')
-    outfile.write(str(colors) ) 
-    outfile.write('; \n')
-    
-    outfile.write('var totList =')
-    outfile.write(str(totList) ) 
-    outfile.write(';')
 
+    outfile.write('var totColors =')
+    outfile.write(str(colors) )
+    outfile.write('; \n')
+
+    outfile.write('var totList =')
+    outfile.write(str(totList) )
+    outfile.write('; \n')
+
+    outfile.write("var totDict = '")
+    json.dump(totDict, outfile)
+    outfile.write("'")
